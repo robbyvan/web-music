a61d22436b8d1c8eec32c6527fce3247bd9ba64c
Robby <fzpinus@gmail.com>
2018-08-09 17:04:51 +0800

    init: first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style lang="scss" scoped>
+  @import "common/scss/const.scss";
+  #app {
+    color: $color-theme;
+  }
+</style>
