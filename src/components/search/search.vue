2d476ca20ddb38d0d6373c721e5cb967f07c1067
Robby <fzpinus@gmail.com>
2018-08-12 17:51:52 +0800

    feat: search-box

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,15 +1,88 @@
 <template>
-  <div>search Page</div>
+  <div class="search">
+    <div class="search-box-wrapper">
+      <search-box></search-box>
+    </div>
+  </div>
 </template>
 
 <script>
+import SearchBox from 'base/search-box/search-box';
+
 export default {
-  data() {
-    return {};
-  }
+  components: {
+    SearchBox,
+  },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import 'common/scss/const.scss';
+@import 'common/scss/mymixin.scss';
 
+.my-search {
+  .search-box-wrapper {
+    margin: 20px;
+  }
+  .shortcut-wrapper {
+    position: fixed;
+    top: 178px;
+    bottom: 0;
+    width: 100%;
+    .shortcut {
+      height: 100%;
+      overflow: hidden;
+      .hot-key {
+        // height: 170px;
+        // overflow: hidden;
+        margin: 0 20px 20px 20px;
+        .title {
+          margin-bottom: 20px;
+          font-size: $font-size-medium;
+          color: $color-text-l;
+        }
+        .item {
+          display: inline-block;
+          padding: 5px 10px;
+          margin: 0 20px 10px 0;
+          border-radius: 6px;
+          background: $color-highlight-background;
+          font-size: $font-size-medium;
+          color: $color-text-d;
+          &.special {
+            color: rgba(255, 255, 255, 0.7);
+            // border: 1px solid #fc4524;
+          }
+        }
+      }
+      .search-history {
+        position: relative;
+        margin: 0 20px;
+        .title {
+          display: flex;
+          align-items: center;
+          height: 40px;
+          font-size: $font-size-medium;
+          color: $color-text-l;
+          .text {
+            flex: 1;
+          }
+          .clear {
+            @include extend-click();
+            .icon-clear {
+              font-size: $font-size-medium;
+              color: $color-text-d;
+            }
+          }
+        }
+      }
+    }
+  }
+  .search-result {
+    position: fixed;
+    width: 100%;
+    top: 178px;
+    bottom: 0;
+  }
+}
 </style>
